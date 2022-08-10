213b644b80353810a87a5da0dc2f476c721f3455
shanicesauce <shanicesauceda@gmail.com>
2022-08-10 17:52:29 -0500

    subtracted time for wrong answers, scores kinda in local storage

--- js/script.js
+++ js/script.js
@@ -9,52 +9,185 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+var header = document.querySelector("#head");
 var startButton = document.querySelector("#begin");
 var quizTimer = document.querySelector("#counter");
+var highScore = document.querySelector("#highscore");
 var quesionPrompt = document.querySelector(".question-container");
 var question = document.querySelector("#questions");
 var answerChoices = document.querySelectorAll(".btn");
-
-var firstQuestionAsked = {
-    asked: "question will be here",
-    choices: ["choice 1","2","3","4"],
-    answer: 0
-    //answer is # in array
-}
+var gameDone = document.querySelector("#end");
+var endScore =document.querySelector("#gameover");
+var choiceOne  = document.querySelector ('#choiceOne');
+var choiceTwo = document.querySelector ('#choiceTwo');
+var choiceThree = document.querySelector ('#choiceThree');
+var choiceFour = document.querySelector ('#choiceFour');
+var choiceOptions = [choiceOne,choiceTwo,choiceThree,choiceFour];
 
 // timer begins when clicking the button to start quiz
 startButton.addEventListener("click",quizCountdown);
 
-function quizCountdown () {
-    var countStart = 80;
 
-    quizTimer.innerHTML = countStart;
+var gameScore = 0;
+var countStart = 80;
+function quizCountdown () {
+   
+
+    quizTimer.textContent =  countStart;
 
     var quizStart = setInterval(function(){
         countStart --;
-        quizTimer.innerHTML = countStart;
-       }
-    ,1000);
+        quizTimer.textContent = countStart;
+    
+        if (countStart == 0) {
+        countStart.textContent = '';
+        clearInterval(quizStart);
+        }
+        
+        
+},1000);
 
+    
+quizQuestion();
       // if timer hits 0 quiz over
     //if wrong answer - 10 seconds
-
+    highScore.classList.add("hide");
     startButton.classList.add("hide");
+    header.classList.add("hide");
     quesionPrompt.classList.remove("hide");
-    quizQuestion();
+ 
 } 
 
 
+var firstQuestionAsked = {
+    asked: "question will be here",
+    choices: ["choice 1","2","3","4"],
+    answer: "2"
+};
 // ask each question and present the answers
 function quizQuestion (){
-    question.innerHTML = firstQuestionAsked.asked;
+ console.log (firstQuestionAsked.asked);
+ question.textContent = firstQuestionAsked.asked
 
 
+ for (var i = 0; i < choiceOptions.length; i++) {
+    choiceOptions[i].textContent = firstQuestionAsked.choices[i]
+    choiceOptions[i].setAttribute("onclick","selectedAnswer(this)")
+ }
 }
+
+function selectedAnswer(answer){
+    var choiceAns = answer.textContent;
+    var correctAnswer = firstQuestionAsked.answer;
+    if (choiceAns == correctAnswer){
+      alert ("correct!")
+      gameScore= + 10;
+      console.log(gameScore);
+      
+    }
+    else {
+        alert ("wrong!")
+        countStart = countStart -10;
+        
+    }
+secondQuestion() 
+   
+}
+
+    var secondQuestionAsked = {
+    asked: "second question will be here",
+    choices: ["choice 1","2","choice 3","4"],
+    answer: "choice 1"
+};
+
+function secondQuestion() {
+    console.log (secondQuestionAsked.asked);
+    question.textContent = secondQuestionAsked.asked
+
+
+    for (var i = 0; i < choiceOptions.length; i++) {
+        choiceOptions[i].textContent = secondQuestionAsked.choices[i]
+        choiceOptions[i].setAttribute("onclick","selectAnswer(this)")
+     }
+    }
+    
+    function selectAnswer(answer){
+        var choiceAns = answer.textContent;
+        var correctAnswer = secondQuestionAsked.answer;
+        console.log (correctAnswer);
+        if (choiceAns == correctAnswer){
+          alert ("correct!");
+          gameScore = gameScore + 10;
+          console.log(gameScore);
+        }
+        else {
+            alert ("wrong!");
+            countStart = countStart -10;
+            
+        }
+     thirdQuestion()
+    };
+
+var thirdQuestionAsked = {
+     asked: "third question will be here",
+    choices: ["choice 1","2"," 3","choice 4"],
+    answer: "2"
+};
+
+
+function thirdQuestion() {
+    console.log (thirdQuestionAsked.asked);
+    question.textContent = thirdQuestionAsked.asked
+        
+   
+    for (var i = 0; i < choiceOptions.length; i++) {
+        choiceOptions[i].textContent = thirdQuestionAsked.choices[i]
+        choiceOptions[i].setAttribute("onclick","selectAnsr(this)")
+     }
+    }
+    
+    function selectAnsr(answer){
+        var choiceAns = answer.textContent;
+        var correctAnswer = thirdQuestionAsked.answer;
+        if (choiceAns == correctAnswer){
+          alert ("correct!");
+          gameScore = gameScore + 10;
+          console.log(gameScore);
+        }
+        else {
+            alert ("wrong!");
+            countStart = countStart -10;
+        }
+    gameOver();
+ };
 
 function quizPoints () {
+    localStorage.setItem ("gameScore",JSON.parse(gameScore));
+
+    var finalScore = localStorage.getItem("gameScore");
+    console.log (finalScore);
+    if (!finalScore) {
+        return false; }
+
+  var scores = document.createElement("input");
+        scores.className="score"
+        endScore.appendChild (scores);
+
+    
+
 
 }
+
+function gameOver (){
+    
+    gameDone.textContent = "Game Over!";
+    quesionPrompt.classList.add("hide");
+
+    quizPoints ()
+   //when clock hit 0 or answered all questions
+   //total score of correct answers and display, allow to save name and score  
+}
+
 
 //make a quiz
 //click button, start timer and show first question
